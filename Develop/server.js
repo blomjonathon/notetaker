b8adac97cd56e0845762f1a508f530b9801e940d
Jonathon Blom <blomjonathon@gmail.com>
2024-02-02 09:32:33 -0600

    made changes to server.js, added comment

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
+
+// require in needed packages and files
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const db = require("./db/db.json");
 const { v4: uuidv4 } = require('uuid');
 
 const PORT = 3001;
